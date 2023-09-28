75eeb26bba956ab9cec78fb2e7becc8bb7726375
ahmed.yar.zahid <ahmed.yar.zahid@hotmail.com>
2023-09-29 00:38:14 +0500

    Added navbar.

---
+++ resources/views/find-distance.blade.php
@@ -0,0 +1,32 @@
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark">
+    <div class="site-logo">
+        <img src="logo.png">
+    </div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">
+                    Distance Calculator
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    Find Distances
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<body>
+</body>
+</html>
